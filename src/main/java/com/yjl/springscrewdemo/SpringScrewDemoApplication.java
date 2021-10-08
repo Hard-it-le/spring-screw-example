9e5f7d72fb34930e73a381c7b573d696d46c60c2
994344562@qq.com <994344562@qq.com>
2021-10-08 15:45:18 +0800

    备份

---
+++ src/main/java/com/yjl/springscrewdemo/SpringScrewDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yjl.springscrewdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScrewDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringScrewDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yjl/springscrewdemo/SpringScrewDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjl.springscrewdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringScrewDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
