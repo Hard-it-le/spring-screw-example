a8d02f4313bbe441ed8ff353ecb82a9ae9e34614
yujiale <yu18371919563@163.com>
2023-01-16 14:19:29 +0800

    feat：spring boot 整合  Screw 示例代码

---
+++ src/main/java/com/screw/springscrewexample/SpringScrewExampleApplication.java
@@ -0,0 +1,13 @@
+package com.screw.springscrewexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScrewExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringScrewExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/screw/springscrewexample/SpringScrewExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.screw.springscrewexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringScrewExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
